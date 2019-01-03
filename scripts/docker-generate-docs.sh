43f48dcb8659e35e2e59f82dd6fe8ca4e6c6ed48
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-01-03 22:56:35 +0000

    docker build

--- scripts/docker-generate-docs.sh
+++ scripts/docker-generate-docs.sh
@@ -3,6 +3,6 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build ${THIS_DIR}/../doc_builder -t tk-docs && \
-docker run --rm  --mount type=bind,source="${THIS_DIR}/..",target=/app tk-docs \
-/app/doc_builder/scripts/build_docs.sh --url=http://localhost --url-path=/tmp/foo --source=/app/docs --output=/app/_build && \
-echo "Documentation built in ${THIS_DIR}/_build/index.html"
+docker run --mount type=bind,source="${THIS_DIR}/..",target=/app tk-docs \
+/app/doc_builder/scripts/build_docs.sh --url=http://localhost --url-path=${THIS_DIR}/../_build --source=/app/docs --output=/app/_build && \
+echo "Documentation built in ${THIS_DIR}/../_build/index.html"
